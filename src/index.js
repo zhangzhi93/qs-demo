5a37285aa873e810d291d5cab312264f60be8768
Janz <zhangzhi93@hotmail.com>
2018-06-06 23:06:38 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Qs from 'qs';
+
+const obj = {
+  name:'janz',
+  age:26
+}
+console.log(Qs.stringify(obj));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+  var path = require('path');
+  var webpack = require("webpack");
+
+  module.exports = {
+    entry: './src/index.js',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    //mode: 'production',
+    module: {
+      rules: [{
+        test: /\.js$/,
+        type: "javascript/auto",
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        use: "babel-loader"
+      }]
+    }
+  }
