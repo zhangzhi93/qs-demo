861c63a3e02ad41b6e0c535f91714e79f8bdfdcd
zhangzhi <zhangzhi93@hotmail.com>
2018-06-07 17:40:56 +0800

    page show

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import Qs from 'qs';
+import {
+  getElementById
+} from './utils';
+
+let textarea = document.createElement('textarea');
+textarea.setAttribute('id', 'before_value');
+textarea.style.width = '250px';
+textarea.style.height = '100px';
+let button = document.createElement('button');
+button.textContent = '转换';
+button.setAttribute('id', 'btn_transform');
+let showDiv = document.createElement('div');
+showDiv.setAttribute('id', 'after_value');
+document.body.appendChild(textarea);
+document.body.appendChild(button);
+document.body.appendChild(showDiv);
+getElementById('btn_transform').onclick = function() {
+  let textValue = getElementById('before_value').value;
+  let textJson = JSON.parse(textValue || '{}');
+  getElementById('after_value').innerHTML = Qs.stringify(textJson);
+}
 
 const obj = {
-  name:'janz',
-  age:26
+  name: 'janz',
+  age: 26
 }
 console.log(Qs.stringify(obj));
---
+++ src/utils.js
@@ -0,0 +1,13 @@
+export function getElementById(id) {
+  return document.getElementById(id);
+}
+
+//  let button = document.createElement('button');
+//  button.textContent = '转换';
+//  button.setAttribute('id', 'btn_transform');
+//  {"name":"janz","age":23,"sex":"male"}
+
+export function createElements(str, dom) {
+  str = '<button id="btn" class="middle-size">转换</button>';
+
+}
