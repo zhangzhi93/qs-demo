4cb76ba575d835e5d8cbf7ff3df1030e64c6c1f0
Janz <zhangzhi93@hotmail.com>
2020-09-16 16:52:50 +0800

    依赖升级，语法修改

--- src/index.js
+++ src/index.js
@@ -5,30 +5,32 @@ import {
 } from './utils';
 import './style.less';
 
-let container = createElements('<div class="container"></div>');
+const defaultObjStr = '{"a":1,"b":2}';
+
+const container = createElements('<div class="container"></div>');
 createElements('<h2>Qs 转换:</h2>', container);
-let textarea = createElements('<textarea class="textarea" placeholder="请输入符合标准的json"></textarea>', container);
-textarea.value = '{"a":1,"b":2}';
-let btnDiv = createElements('<div class="div-btn"></div>', container);
-let button = createElements('<button>转换</button>', btnDiv);
-let showDiv = createElements('<div class="div-show"></div>', container);
+const textarea = createElements('<textarea class="textarea" placeholder="请输入符合标准的json"></textarea>', container);
+textarea.value = defaultObjStr;
+const btnDiv = createElements('<div class="div-btn"></div>', container);
+const button = createElements('<button>转换</button>', btnDiv);
+const showDiv = createElements('<div class="div-show"></div>', container);
 
 
-let URLcontainer = createElements('<div class="container"></div>');
+const URLcontainer = createElements('<div class="container"></div>');
 createElements('<h2>URLSearchParams 转换:</h2>', URLcontainer);
-let URLtextarea = createElements('<textarea class="textarea" placeholder="请输入符合标准的json"></textarea>', URLcontainer);
-URLtextarea.value = '{"a":1,"b":2}';
-let URLbtnDiv = createElements('<div class="div-btn"></div>', URLcontainer);
-let URLbutton = createElements('<button>转换</button>', URLbtnDiv);
-let URLshowDiv = createElements('<div class="div-show"></div>', URLcontainer);
+const URLtextarea = createElements('<textarea class="textarea" placeholder="请输入符合标准的json"></textarea>', URLcontainer);
+URLtextarea.value = defaultObjStr;
+const URLbtnDiv = createElements('<div class="div-btn"></div>', URLcontainer);
+const URLbutton = createElements('<button>转换</button>', URLbtnDiv);
+const URLshowDiv = createElements('<div class="div-show"></div>', URLcontainer);
 
-button.onclick = function() {
-  let textValue = textarea.value;
-  let textJson = JSON.parse(textValue || '{}');
+button.onclick = function () {
+  const textValue = textarea.value;
+  const textJson = JSON.parse(textValue || '{}');
   showDiv.innerHTML = Qs.stringify(textJson);
 }
 
-URLbutton.onclick = function() {
+URLbutton.onclick = function () {
   let URLtextValue = URLtextarea.value;
   let URLtextJson = JSON.parse(URLtextValue || '{}');
   let params = new URLSearchParams();
